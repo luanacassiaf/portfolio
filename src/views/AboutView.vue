7f3319a8bd6097089f188020d8ba651613cb861b
Luana Freitas <luanacassiaf@gmail.com>
2025-04-25 17:39:46 -0300

    style: Seção Sobre

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,33 +1,56 @@
 <template>
-	<section class="resume-section" id="about">
-		<div class="resume-section-content">
-			<h1 class="mb-0">
-				Luana
-				<span class="text-primary">Freitas</span>
-			</h1>
-			<div class="subheading mb-5">
-				<a href="mailto:luanacassiaf@gmail.com">luanacassiaf@gmail.com</a>
-			</div>
-			<p class="lead mb-5">Mussum Ipsum, cacilds vidis litro abertis. Nulla id gravida magna, ut semper sapien. In
-				elementis mé pra quem é amistosis quis leo. Suco de cevadiss deixa as pessoas mais interessantis. Praesent
-				malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus.</p>
-			<div class="social-icons">
-				<a class="social-icon" href="https://www.linkedin.com/in/luanacassiaf/" target="_blank">
-					<i class="bi bi-linkedin"></i>
-				</a>
-				<a class="social-icon" href="https://github.com/luanacassiaf" target="_blank">
-					<i class="bi bi-github"></i>
-				</a>
-			</div>
-		</div>
-	</section>
-</template>
+  <section class="resume-section" id="about">
+    <div class="resume-section-content">
+      <h1 class="mb-0">
+        Luana
+        <span class="text-primary">Freitas</span>
+      </h1>
 
-<style scoped></style>
+      <div class="subheading mb-5">
+        <a href="mailto:luanacassiaf@gmail.com">luanacassiaf@gmail.com</a>
+      </div>
+
+      <p class="lead">
+        Tenho grande afinidade por desenvolvimento full stack de softwares web,
+        com experiência em PHP, JavaScript, HTML, CSS, jQuery, Vue.js e bancos
+        de dados SQL.
+      </p>
+      <p class="lead">
+        Possuo habilidades em testes de software, Linux e controle de versão com
+        Git, além de conhecimentos em Node.js e MongoDB.
+      </p>
+      <p class="lead">
+        Dedico-me a assegurar entregas de qualidade em tempo hábil a partir da
+        automação estruturada nos testes.
+      </p>
+      <p class="lead mb-5">
+        Valorizo o trabalho em equipe, cultivando boas relações através da
+        empatia e estando aberta a críticas construtivas. Abordo desafios com
+        determinação e proatividade.
+      </p>
+
+      <div class="social-icons">
+        <a
+          class="social-icon"
+          href="https://www.linkedin.com/in/luanacassiaf/"
+          target="_blank"
+        >
+          <i class="bi bi-linkedin"></i>
+        </a>
+        <a
+          class="social-icon"
+          href="https://github.com/luanacassiaf"
+          target="_blank"
+        >
+          <i class="bi bi-github"></i>
+        </a>
+      </div>
+    </div>
+  </section>
+</template>
 
 <script>
 export default {
-	created() {
-	}
-}
+  created() {},
+};
 </script>
