0f69ce76dfe8e6c97d131bd2e515de6a0ac490c2
Luana Freitas <luanacassiaf@gmail.com>
2025-04-28 16:18:24 -0300

    fix: Componente do menu

---
+++ src/Navbar.vue
@@ -0,0 +1,100 @@
+<template>
+  <nav
+    class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top"
+    id="sideNav"
+  >
+    <a class="navbar-brand js-scroll-trigger">
+      <span class="d-block d-lg-none">Luana Freitas</span>
+      <span class="d-none d-lg-block">
+        <img
+          class="img-fluid img-profile rounded-circle mx-auto mb-2"
+          src="./assets/cat.png"
+          alt="..."
+        />
+      </span>
+    </a>
+
+    <button
+      class="navbar-toggler"
+      type="button"
+      @click="toggleMenu"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Colapso do menu -->
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <RouterLink
+            to="/"
+            class="nav-link js-scroll-trigger"
+            @click="closeMenu"
+            >Sobre</RouterLink
+          >
+        </li>
+        <li class="nav-item">
+          <RouterLink
+            to="/experience"
+            class="nav-link js-scroll-trigger"
+            @click="closeMenu"
+            >Experiência</RouterLink
+          >
+        </li>
+        <li class="nav-item">
+          <RouterLink
+            to="/education"
+            class="nav-link js-scroll-trigger"
+            @click="closeMenu"
+            >Educação</RouterLink
+          >
+        </li>
+        <li class="nav-item">
+          <RouterLink
+            to="/skills"
+            class="nav-link js-scroll-trigger"
+            @click="closeMenu"
+            >Habilidades</RouterLink
+          >
+        </li>
+        <li class="nav-item">
+          <RouterLink
+            to="/certificates"
+            class="nav-link js-scroll-trigger"
+            @click="closeMenu"
+            >Certificados</RouterLink
+          >
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import Collapse from "bootstrap/js/dist/collapse";
+
+let collapseInstance;
+
+onMounted(() => {
+  const navbar = document.getElementById("navbarResponsive");
+  if (navbar) {
+    collapseInstance = new Collapse(navbar, {
+      toggle: false,
+    });
+  }
+});
+
+const toggleMenu = () => {
+  if (collapseInstance) {
+    collapseInstance.toggle();
+  }
+};
+
+const closeMenu = () => {
+  if (collapseInstance) {
+    collapseInstance.hide();
+  }
+};
+</script>
