033e24b628b3bd32623648f6fa5bb3d0d1d7a7fd
Luana Freitas <luanacassiaf@gmail.com>
2025-04-28 15:38:07 -0300

    style: Cor e ícones

--- src/main.js
+++ src/main.js
@@ -5,14 +5,16 @@ import router from './router'
 import "bootstrap"
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import "./assets/styles.css"
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
-library.add(fas, fab)
+library.add(fas, fab, far)
 
 const app = createApp(App)
 
