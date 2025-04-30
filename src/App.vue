7a54a49c6bcf411898f5110cdceb187693ecf3b5
Luana Freitas <luanacassiaf@gmail.com>
2025-04-30 17:41:20 -0300

    style: Transition

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,25 @@
     />
 
     <Navbar />
-    <RouterView />
+    <Transition name="fade" mode="out-in">
+      <RouterView />
+    </Transition>
   </div>
 </template>
 
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 import Navbar from "./Navbar.vue";
 </script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
